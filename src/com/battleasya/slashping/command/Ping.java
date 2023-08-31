9437c941a90317381d45b5aa2dc3546c886470ae
Dennie Mok <mokdennie26@gmail.com>
2023-08-31 11:07:16 +1000

    Moved getPing method to util

--- src/com/battleasya/slashping/command/Ping.java
+++ src/com/battleasya/slashping/command/Ping.java
@@ -10,9 +10,6 @@ import org.bukkit.command.CommandSender;
 // DEPRECATED: import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
 public class Ping implements CommandExecutor {
 
     private final SlashPing plugin;
@@ -21,24 +18,31 @@ public class Ping implements CommandExecutor {
         this.plugin = plugin;
     }
 
-    private Method getHandleMethod;
-
-    private Field pingField;
-
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String s, String[] args) {
 
         if (!(sender instanceof Player)) {
+            sender.sendMessage("This is a player-only command!");
+            return true;
+        }
+
+        boolean canPingSelf = Util.canPingSelf(sender);
+        boolean canPingOthers = Util.canPingOthers(sender);
+
+        if (!canPingSelf && !canPingOthers) {
+            Util.sendMessage(sender, plugin.config.noPermission);
             return true;
         }
 
         if (args.length == 0) {
 
-            if (sender.hasPermission("ping.self")) {
+            if (canPingSelf) {
 
-                Player p = Bukkit.getPlayer(sender.getName());
+                Player player = Bukkit.getPlayer(sender.getName());
 
-                String ping = String.valueOf(getPing(p));
+                String ping = String.valueOf(Util.getPing(player
+                        , plugin.config.pingMultiplier
+                        , plugin.config.pingOffset));
                 // DEPRECATED: int ping = (int) (((((CraftPlayer) p).getHandle()).ping) * plugin.config.pingOffset);
 
                 Util.sendMessage(sender, plugin.config.pingSelf
@@ -46,7 +50,7 @@ public class Ping implements CommandExecutor {
 
             } else {
 
-                Util.sendMessage(sender, plugin.config.noPermission);
+                Util.sendMessage(sender, plugin.config.noPermissionSelf);
 
             }
 
@@ -56,14 +60,17 @@ public class Ping implements CommandExecutor {
 
         if (args.length == 1) {
 
-            if (sender.hasPermission("ping.others")) {
+            if (canPingOthers) {
 
-                Player p = Bukkit.getPlayer(args[0]);
+                Player player = Bukkit.getPlayer(args[0]);
 
-                if (p != null && (!p.hasPermission("ping.exempt") || sender.hasPermission("ping.ignoreexempt"))) {
+                if (player != null && (!player.hasPermission("ping.exempt")
+                        || sender.hasPermission("ping.ignoreexempt"))) {
 
-                    String playerName = p.getName();
-                    String ping = String.valueOf(getPing(p));
+                    String playerName = player.getName();
+                    String ping = String.valueOf(Util.getPing(player
+                            , plugin.config.pingMultiplier
+                            , plugin.config.pingOffset));
                     // DEPRECATED: int ping = (int) (((((CraftPlayer) p).getHandle()).ping) * plugin.config.pingOffset);
 
                     Util.sendMessage(sender, plugin.config.pingOthers
@@ -78,7 +85,7 @@ public class Ping implements CommandExecutor {
 
             } else {
 
-                Util.sendMessage(sender, plugin.config.noPermission);
+                Util.sendMessage(sender, plugin.config.noPermissionOthers);
 
             }
 
@@ -86,76 +93,8 @@ public class Ping implements CommandExecutor {
 
         }
 
-        Util.sendMessage(sender, plugin.config.incorrectSyntax);
+        Util.sendMessage(sender, plugin.config.pingSyntax);
         return true;
-
-    }
-
-    public int getPing(Player p) {
-
-        switch (plugin.version) {
-            case 1:
-                return getPingLegacy(p);
-            case 2:
-                return getPingModern(p);
-            default: /* unknown = 0 */
-                int legacyResult = getPingLegacy(p); /* try legacy first */
-                if (legacyResult == -1) {
-                    plugin.version = 2;
-                    return getPingModern(p);
-                } else {
-                    plugin.version = 1;
-                    return legacyResult;
-                }
-        }
-
-    }
-
-    /* https://www.spigotmc.org/threads/get-player-ping-with-reflection.147773/ */
-    /* credits to konsolas for the reflection system */
-    public int getPingLegacy(Player p) {
-
-        try {
-
-            if (getHandleMethod == null) {
-                getHandleMethod = p.getClass().getDeclaredMethod("getHandle");
-                getHandleMethod.setAccessible(true);
-            }
-
-            Object entityPlayer = getHandleMethod.invoke(p);
-
-            if (pingField == null) {
-                pingField = entityPlayer.getClass().getDeclaredField("ping");
-                pingField.setAccessible(true);
-            }
-
-            int ping = (int) (pingField.getInt(entityPlayer)
-                    * plugin.config.pingMultiplier + plugin.config.pingOffset);
-
-            return Math.max(ping, 0); /* greater of the 2 values */
-
-        } catch (Exception e) {
-
-            return -1;
-
-        }
-
-    }
-
-    public int getPingModern(Player p) {
-
-        try {
-
-            int ping = (int) (p.getPing()
-                    * plugin.config.pingMultiplier + plugin.config.pingOffset);
-
-            return Math.max(ping, 0); /* greater of the 2 values */
-
-        } catch (Exception e) {
-
-            return -1;
-
-        }
 
     }
 
